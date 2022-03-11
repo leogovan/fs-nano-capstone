65f047fcf29b040b0d8cc204b90ff2c22767c00a
leogovan <leogovan@yahoo.co.uk>
2022-03-11 21:33:54 +0000

    Refactor app.py

--- backend/app.py
+++ backend/app.py
@@ -39,61 +39,69 @@ def create_app(test_config=None):
 	#----------------------------------------------------------------------------#
 
 	"""
-	Get and Create movies
+	Get movies
 	"""
 
-	@app.route('/movies', methods=['GET', 'POST'])
-	def movies_get_or_post():
-		if request.method == 'GET':
+	@app.route('/movies', methods=['GET'])
+	def get_movies():
+		try:
 			selection = Movie.query.all()
 			
 			movies = []
-			
+				
 			for movie in selection:
 				movies.append(movie.format())
-			
+				
 			# Combo of .loads and .dumps strips out the '\' that was occuring for every value
 			movies = json.loads(json.dumps(movies))
-			
+				
 			return jsonify({
 				'success': True,
 				'movies': movies,
 				'total_movies': len(selection)
 			})
 		
-		else:
-			body = request.get_json()
+		except:
+			abort(500)
+	
+	"""
+	Create movie
+	"""
+	
+	@app.route('/movies', methods=['POST'])
+	def post_movie():
+		body = request.get_json()
 		
-			new_movie_name = body.get('movie_name')
-			new_genre = body.get('genre')
-			new_release_date = body.get('release_date')
-			new_director = body.get('director')
+		new_movie_name = body.get('movie_name')
+		new_genre = body.get('genre')
+		new_release_date = body.get('release_date')
+		new_director = body.get('director')
 
-			movie = Movie(
-				movie_name=new_movie_name,
-				genre=new_genre,
-				release_date=new_release_date,
-				director=new_director
-			)
+		movie = Movie(
+			movie_name=new_movie_name,
+			genre=new_genre,
+			release_date=new_release_date,
+			director=new_director
+		)
 
-			try:
-				movie.insert()
+		try:
+			movie.insert()
 
-				return jsonify({
-					'success': True
-				}), 200
+			return jsonify({
+				'success': True
+			}), 200
 			
-			except:
-				movie.undo()
-				abort(500)
+		except:
+			movie.undo()
+			abort(500)
 
 
 	"""
-	Delete and Update Movies
+	Delete movie
 	"""
 
-	@app.route('/movies/<int:movie_id>', methods=['DELETE', 'PATCH'])
-	def movies_delete_or_patch(movie_id):
+	@app.route('/movies/<int:movie_id>', methods=['DELETE'])
+	def delete_movie(movie_id):
 		
 		movie = Movie.query.get(movie_id)
 
@@ -101,92 +109,109 @@ def create_app(test_config=None):
 			print("movie is None")
 			abort(422)
 		
-		if request.method == 'DELETE':
-			try:
-				movie.delete()
-
-				return jsonify({
-					'success': True,
-					'deleted_movie_id': movie_id
-				}), 200
-				
-			except:
-				abort(422)
-		
-		else:
-			try:
-				body = request.get_json()
-				
-				if body is None:
-					print("body is None")
-					abort(422)
-				
-				else:
-					release_date_update = body.get('release_date')
-
-					movie.release_date = release_date_update
-
-					movie.update()
-					
-					return jsonify({
-						"success": True,
-						'patched_movie_id': movie_id
-					}), 200
-			
-			except:
-				abort(422)
-
-
-	"""
-	Get and create actors
-	"""
-
-	@app.route('/actors', methods=['GET', 'POST'])
-	def actors_get_or_post():
-		if request.method == 'GET':
-			selection = Actor.query.all()
-
-			actors = []
-
-			for actor in selection:
-				actors.append(actor.format())
-
-			# Combo of .loads and .dumps strips out the '\' that was occuring for every value
-			actors = json.loads(json.dumps(actors))
+		try:
+			movie.delete()
 
 			return jsonify({
 				'success': True,
-				'actors': actors,
-				'total_actors': len(selection)
-			})
+				'deleted_movie_id': movie_id
+			}), 200
+				
+		except:
+			abort(422)
+
+
+	"""
+	Update movie
+	"""
+
+	@app.route('/movies/<int:movie_id>', methods=['PATCH'])
+	def patch_movie(movie_id):
 		
-		else:
+		movie = Movie.query.get(movie_id)
+
+		if movie is None:
+			print("movie is None")
+			abort(422)
+		
+		try:
 			body = request.get_json()
+				
+			if body is None:
+				print("body is None")
+				abort(422)
+				
+			else:
+				release_date_update = body.get('release_date')
 
-			new_actor_name = body.get('actor_name')
-			new_phone = body.get('phone')
-			new_age = body.get('age')
-			new_gender = body.get('gender')
-			new_image_link = body.get('image_link')
+				movie.release_date = release_date_update
 
-			actor = Actor(
-				actor_name=new_actor_name,
-				phone=new_phone,
-				age=new_age,
-				gender=new_gender,
-				image_link=new_image_link
-			)
-
-			try:
-				actor.insert()
-
+				movie.update()
+					
 				return jsonify({
-					'success': True
+					"success": True,
+					'patched_movie_id': movie_id
 				}), 200
 			
-			except:
-				actor.undo()
-				abort(500)
+		except:
+			abort(422)
+
+
+	"""
+	Get actors
+	"""
+
+	@app.route('/actors', methods=['GET'])
+	def get_actors():
+		selection = Actor.query.all()
+
+		actors = []
+
+		for actor in selection:
+			actors.append(actor.format())
+
+		# Combo of .loads and .dumps strips out the '\' that was occuring for every value
+		actors = json.loads(json.dumps(actors))
+
+		return jsonify({
+			'success': True,
+			'actors': actors,
+			'total_actors': len(selection)
+		})
+
+
+	"""
+	Create actor
+	"""
+
+	@app.route('/actors', methods=['POST'])
+	def post_actor():
+		body = request.get_json()
+
+		new_actor_name = body.get('actor_name')
+		new_phone = body.get('phone')
+		new_age = body.get('age')
+		new_gender = body.get('gender')
+		new_image_link = body.get('image_link')
+
+		actor = Actor(
+			actor_name=new_actor_name,
+			phone=new_phone,
+			age=new_age,
+			gender=new_gender,
+			image_link=new_image_link
+		)
+
+		try:
+			actor.insert()
+
+			return jsonify({
+				'success': True
+			}), 200
+			
+		except:
+			actor.undo()
+			abort(500)
 
 
 	"""
@@ -215,55 +240,59 @@ def create_app(test_config=None):
 
 
 	"""
-	Get and create roles
+	Get roles
 	"""
 
-	@app.route('/roles', methods=['GET', 'POST'])
-	def roles_get_or_post():
-		if request.method == 'GET':
-			selection = Role.query.all()
+	@app.route('/roles', methods=['GET'])
+	def get_roles():
+		selection = Role.query.all()
 
-			roles = []
+		roles = []
 
-			for role in selection:
-				roles.append(role.format())
+		for role in selection:
+			roles.append(role.format())
 
-			# Combo of .loads and .dumps strips out the '\' that was occuring for every value
-			roles = json.loads(json.dumps(roles))
+		# Combo of .loads and .dumps strips out the '\' that was occuring for every value
+		roles = json.loads(json.dumps(roles))
+
+		return jsonify({
+			'success': True,
+			'roles': roles,
+			'total_roles': len(selection)
+		}), 200
+	
+	"""
+	Create role
+	"""
+
+	@app.route('/roles', methods=['POST'])
+	def post_role():
+		body = request.get_json()
+
+		new_role_number = body.get('role_number')
+		new_role_type_id = body.get('role_type_id')
+		new_movie_id = body.get('movie_id')
+
+		role = Role(
+			role_number=new_role_number,
+			role_type_id=new_role_type_id,
+			movie_id=new_movie_id
+		)
+
+		try:
+			role.insert()
 
 			return jsonify({
-				'success': True,
-				'roles': roles,
-				'total_roles': len(selection)
+				'success': True
 			}), 200
-		
-		else:
-			body = request.get_json()
-
-			new_role_number = body.get('role_number')
-			new_role_type_id = body.get('role_type_id')
-			new_movie_id = body.get('movie_id')
-
-			role = Role(
-				role_number=new_role_number,
-				role_type_id=new_role_type_id,
-				movie_id=new_movie_id
-			)
-
-			try:
-				role.insert()
-
-				return jsonify({
-					'success': True
-				}), 200
 			
-			except:
-				role.undo()
-				abort(500)
+		except:
+			role.undo()
+			abort(500)
 
 
 	"""
-	Delete roles
+	Delete role
 	"""
 	@app.route('/roles/<int:role_id>', methods=['DELETE'])
 	def roles_delete(role_id):
@@ -286,60 +315,63 @@ def create_app(test_config=None):
 				abort(500)
 
 	"""
-	Get and create commitments
+	Get commitments
+	"""
+
+	@app.route('/commitments', methods=['GET'])
+	def get_commitments():
+		selection = Commitment.query.all()
+
+		commitments = []
+
+		for commitment in selection:
+			commitments.append(commitment.format())
+
+		# Combo of .loads and .dumps strips out the '\' that was occuring for every value
+		commitments = json.loads(json.dumps(commitments))
+
+		return jsonify({
+			'success': True,
+			'commitments': commitments,
+			'total_sommitments': len(selection)
+		}), 200
+
+	"""
+	Create commitment
 	"""
 
 	@app.route('/commitments', methods=['GET', 'POST'])
-	def commitments_get_or_post():
-		if request.method == 'GET':
-			selection = Commitment.query.all()
+	def post_commitment():
+		body = request.get_json()
 
-			commitments = []
+		new_start_date = body.get('start_date')
+		new_end_date = body.get('end_date')
+		new_movie_id = body.get('movie_id')
+		new_actor_id = body.get('actor_id')
+		new_role_type_id = body.get('role_type_id')
+			
 
-			for commitment in selection:
-				commitments.append(commitment.format())
+		commitment = Commitment(
+			start_date=new_start_date,
+			end_date=new_end_date,
+			movie_id=new_movie_id,
+			actor_id=new_actor_id,
+			role_type_id=new_role_type_id
+		)
 
-			# Combo of .loads and .dumps strips out the '\' that was occuring for every value
-			commitments = json.loads(json.dumps(commitments))
+		try:
+			commitment.insert()
 
 			return jsonify({
-				'success': True,
-				'commitments': commitments,
-				'total_sommitments': len(selection)
+				'success': True
 			}), 200
-		else:
-			body = request.get_json()
-
-			new_start_date = body.get('start_date')
-			new_end_date = body.get('end_date')
-			new_movie_id = body.get('movie_id')
-			new_actor_id = body.get('actor_id')
-			new_role_type_id = body.get('role_type_id')
 			
-
-			commitment = Commitment(
-				start_date=new_start_date,
-				end_date=new_end_date,
-				movie_id=new_movie_id,
-				actor_id=new_actor_id,
-				role_type_id=new_role_type_id
-				
-			)
-
-			try:
-				commitment.insert()
-
-				return jsonify({
-					'success': True
-				}), 200
-			
-			except:
-				commitment.undo()
-				abort(500)
-
+		except:
+			commitment.undo()
+			abort(500)
 
 	"""
-	Delete commitments
+	Delete commitment
 	"""
 	@app.route('/commitments/<int:commitment_id>', methods=['DELETE'])
 	def commitments_delete(commitment_id):
