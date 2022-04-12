33ab166b0b8d4c0a3c604b88ee7829f6a9029406
Leo Govan <leogovan@yahoo.co.uk>
2022-04-12 17:38:09 +0100

    modify manage.py with MIGRATION_DIR env variable

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from backend.app import app
 from backend.models import db
 
-migrate = Migrate(app, db)
+MIGRATION_DIR = os.path.join('backend', 'migrations')
+
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
